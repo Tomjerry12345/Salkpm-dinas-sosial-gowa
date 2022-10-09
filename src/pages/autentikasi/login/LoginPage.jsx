36be8d2f03590cb2f23dd43e4d7f3d2af1be5925
Tomjerry12345 <Andrialfiandi46@gmail.com>
2022-10-09 19:38:16 +0800

    revision-v1.0.2

--- src/pages/autentikasi/login/LoginPage.jsx
+++ src/pages/autentikasi/login/LoginPage.jsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
     <>
       <Grid container>
         <Grid item md={6} className="bg1 fullHeight">
-          <img src="background.jpg" height="580" />
+          {/* <img src="background.jpg" height="580" /> */}
         </Grid>
         <Grid item md={6} className="fullHeight">
           <Stack
